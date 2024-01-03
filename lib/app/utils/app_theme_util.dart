059b3097bd54d8eb6da880972ba6b7c17e7047b3
Nawal <nawalx197@gmail.com>
2024-01-03 13:34:09 +0545

    Remove unused code and add splash screen

---
+++ lib/app/utils/app_theme_util.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:story_vista/app/utils/app_color_util.dart';
+
+class AppThemeUtil {
+  AppThemeUtil._();
+
+  static final ThemeData lightTheme = ThemeData(
+    useMaterial3: true,
+    colorSchemeSeed: AppColorUtil.primaryColor,
+  );
+}
